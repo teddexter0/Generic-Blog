5f9a3ef5e3f6693a83c72bf2f116eaa2e03c35da
teddexter0 <teddexter0@gmail.com>
2025-03-29 19:43:24 +0300

    render is disturbing, tweak a lot trial and error

--- server/app.js
+++ server/app.js
@@ -25,17 +25,17 @@ const saltRounds = 10;
 
 const port = process.env.PORT || 3000;
 
-const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-});
+const db = new pg.Client(
+  process.env.NODE_ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        connectionString:
+          "postgres://postgres:Candidate2019@localhost:5432/blog",
+      }
+);
 
 db.connect()
   .then(() => console.log("Connected to PostgreSQL 🚀"))
