033a77e9b27a4fe73a4200d88a6b2812f881fc14
teddexter0 <teddexter0@gmail.com>
2025-03-29 18:11:54 +0300

    add meta viewport for mobile res. Change app.js and gitignore

--- server/app.js
+++ server/app.js
@@ -26,10 +26,11 @@ const saltRounds = 10;
 const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
-  connectionString:
-    process.env.NODE_ENV === "production"
-      ? process.env.PB_HOST
-      : process.env.DATABASE_URL,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
